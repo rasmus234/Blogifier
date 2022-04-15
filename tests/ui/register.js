1f915720c2e344f84a9aeae4ecd07382060f0002
CKDenni <deni3278@easv.dk>
2022-04-15 20:28:51 +0200

    Add login tests

--- tests/ui/register.js
+++ tests/ui/register.js
@@ -1,18 +1,17 @@
 import {Selector, ClientFunction} from "testcafe";
 
-fixture("Register Fixture").page("http://localhost:5000")
+fixture("/admin/register").page("http://localhost:5000/admin/register")
 
-test(".validation-message exists", async t => {
+test("Error messages when no information is provided", async t => {
     let validationExists = await Selector(".validation-message").exists
     await t.expect(validationExists).notOk()
-
     await t.click("#submit")
 
     validationExists = Selector(".validation-message").exists
     await t.expect(validationExists).ok()
 })
 
-test("Passwords do not match error", async t => {
+test("Error when passwords do not match", async t => {
     await t.typeText("#registerPassword", "stigma")
     await t.typeText("#registerConfirmPassword", "stigmata")
     await t.click("#submit")
@@ -21,12 +20,12 @@ test("Passwords do not match error", async t => {
     await t.expect(validationExists).ok()
 })
 
-test("Successfully create account", async t => {
+test("Create account successfully", async t => {
     await t.typeText("#registerEmail", "stigma@stigma.com")
     await t.typeText("#registerName", "stigmathagod")
     await t.typeText("#registerPassword", "stigma")
     await t.typeText("#registerConfirmPassword", "stigma")
-    await t.useRole(Role.anonymous()).click("#submit")
+    await t.click("#submit")
 
     const location = await ClientFunction(() => document.location.href)
     await t.expect(location()).contains("/admin/login")
